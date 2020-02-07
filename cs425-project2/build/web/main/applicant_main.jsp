0370a2594652b5c0b65102d32d79044bc5b49663
NicRunsAndSwims <60766769+NicRunsAndSwims@users.noreply.github.com>
2020-02-06 22:13:39 -0600

    Add files via upload

---
+++ cs425-project2/build/web/main/applicant_main.jsp
@@ -0,0 +1,20 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:useBean id="applicant" class="edu.jsu.mcis.cs425.project2.BeanApplicant" scope="session" />
+<jsp:setProperty name="applicant" property="username" value="<%= request.getRemoteUser() %>" />
+<% applicant.setUserInfo(); %>
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Applicant Welcome Page</title>
+    </head>
+    
+    <body>
+        <h1>Hello, <jsp:getProperty name="applicant" property="displayname" />! </h1>
+        <a href="<%= request.getContextPath() %>/main/applicant_skills.jsp">Skills</a><br> 
+        <a href="<%= request.getContextPath() %>/public/logout.jsp" target="_self">Log Out</a>
+        <a href="<%= request.getContextPath() %>/main/applicant_jobs.jsp" target="_self">Jobs</a>
+    </body>
+    
+</html>
---
+++ cs425-project2/src/java/edu/jsu/mcis/cs425/project2/BeanApplicant.java
@@ -0,0 +1,80 @@
+package edu.jsu.mcis.cs425.project2;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class BeanApplicant {
+    
+    private String username;
+    private String displayname;
+    private int id;
+    private String[] skills;
+    
+    
+    public void setUserInfo() throws SQLException{
+        
+        Database db = new Database();
+        HashMap<String, String> userinfo = db.getUserInfo(username);
+        id = Integer.parseInt(userinfo.get("id"));
+        displayname = userinfo.get("displayname");
+
+    }
+
+    public String getSkillsList() throws SQLException {
+        
+   Database db = new Database();
+   return ( db.getSkillsListAsHTML(id) );
+   
+    }
+    
+   public void setSkillsList() {
+        
+        Database db = new Database();
+        
+        db.setSkillsList(id, skills);        
+    }
+
+    
+   
+  public String getJobsList() {
+        Database db = new Database();
+        return (db.getJobsAsHTML(id));
+    }
+  
+    
+    
+    
+    public String[] getSkills() {
+        return skills;
+    }
+   
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSkills(String[] skills) {
+        this.skills = skills;
+    }
+
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
